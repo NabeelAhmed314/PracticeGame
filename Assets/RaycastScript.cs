899e83e259dbbf62053b0c36443bbf3cccc5bfa0
Nabeel Ahmed <nabeelrajpoot314@gmail.com>
2020-09-07 15:55:15 +0500

    first

---
+++ Assets/RaycastScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastScript : MonoBehaviour
+{
+    void Update()
+    {
+        Debug.Log("Update");
+        Debug.DrawLine(transform.position, new Vector3(5,5,0),Color.green,0.2f); 
+    }
+}
